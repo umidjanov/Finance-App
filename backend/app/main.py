6bbfbbf4e57c95e4d54a37e86f4ebd5aa59c664a
Fayzulloh Umidjanov <your-email@example.com>
2026-06-13 11:31:43 +0500

    update cors with new vercel url

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,7 +26,10 @@ app.add_middleware(SlowAPIMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://finance-app-nj6h-c2x0qmsat-fayzullohumidjanovs-projects.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
